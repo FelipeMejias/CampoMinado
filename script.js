f08ef45819ca58dc3ab185f9e99e3989c1af7eab
felipemejias <felipe.mejias50@gmail.com>
2022-02-11 19:32:20 -0300

    funcionando com 81 casas

---
+++ script.js
@@ -0,0 +1,131 @@
+//const linhasQtd=prompt('Com quantas linhas quer jogar? cada linha possuirá 10 colunas')
+//const bombasQtd=prompt('Com quantas bombas quer jogar?')
+const lista=[]
+let objeto={}
+const listaBombas=[]
+adicionarBombas()
+printarCasas()
+let contador=0
+
+function adicionarBombas(){
+    for(let k=0; k<81; k++){
+        if(k<20){listaBombas.push('<img src="bomba.jpg">')}
+        else{listaBombas.push(0)}
+    }
+    listaBombas.sort(comparador)
+}
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
+
+function printarCasas(){
+    const container=document.querySelector('.container')
+    let indice=0
+    for(let i=1; i<10; i++){
+        container.innerHTML+=`
+            <div class="linha linha${i}"></div>
+        `
+        const linha=document.querySelector(`.linha${i}`)
+        for(let j=1; j<10; j++){
+
+            objeto={
+                linha:i,
+                coluna:j,
+                verso:null
+            }
+            objeto.verso=listaBombas[indice]
+            indice++
+            lista.push(objeto);
+            
+
+            linha.innerHTML+=`
+                <div onclick="virarCasa('l${i}-${j}')" class="casa l${i}-${j}">
+                
+                    <div class="front-face face">
+                        
+                    </div>
+                    <div class="back-face face">
+                        <p>${objeto.verso}</p>
+                    </div>
+                </div>
+            ` 
+        }
+    }
+}
+
+function virarCasa(lxx){
+    const casaSelecionada=document.querySelector(`.${lxx}`)
+    casaSelecionada.classList.add('virado')
+}
+
+
+
+
+
+function virarTodas(){
+    for(let i=1; i<10; i++){
+        for(let j=1; j<10; j++){
+            virarCasa(`l${i}-${j}`)
+        }
+    }
+}
+
+
+
+function definirVerso(a,b){
+    for(let i=1; i<10; i++){
+        for(let j=1; j<10; j++){
+            for(let k=0; k<81; k++){
+                objetoVez=lista[k]
+                if(objetoVez.linha==i+a && objetoVez.coluna==j+b && objetoVez.verso=='<img src="bomba.jpg">'){
+                    mudarVerso(i,j)
+
+                }
+            }
+        }
+            
+    }
+}
+
+definirVerso(-1,-1)
+definirVerso(-1,0)
+definirVerso(-1,1)
+definirVerso(0,-1)
+definirVerso(0,1)
+definirVerso(1,-1)
+definirVerso(1,0)
+definirVerso(1,1)
+
+
+function mudarVerso(x,y){
+    for(let k=0; k<81; k++){
+        objetoVez=lista[k]
+        if(objetoVez.linha==x && objetoVez.coluna==y){
+            if(objetoVez.verso!='<img src="bomba.jpg">'){
+                const casaNova=document.querySelector(`.l${x}-${y} .back-face p`)
+                casaNova.innerHTML=parseInt(casaNova.innerHTML)+1
+                const numero=casaNova.innerHTML
+                definirCor(numero,casaNova)
+            }
+        }
+    }
+}
+
+function definirCor(numero,casa){
+    if(numero==1){
+        casa.classList.add('azul')
+    }
+    if(numero==2){
+        casa.classList.add('verde')
+    }
+    if(numero==3){
+        casa.classList.add('amarelo')
+    }
+    if(numero==4){
+        casa.classList.add('laranja')
+    }
+    if(numero==5){
+        casa.classList.add('laranja')
+    }
+}
